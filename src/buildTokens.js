cfe679c67f35f078b86a2be644d6f19b1b1d06ed
Jamie <jamieolivermaguire@googlemail.com>
2022-06-25 22:25:26 +0100

    tidy up buildTokens.js

--- src/buildTokens.js
+++ src/buildTokens.js
@@ -1,33 +1,38 @@
 const fs = require('fs')
 const path = require('path')
 
-const parseTokens = (tokens, label) => {
-  const arr = []
-  let name = '';
+/**
+ * Traverses a tree of design tokens whilst converting them into string representations of SCSS variables. 
+ * @param {object} tokensNode an object containing design tokens
+ * @param {string} constructedTokenName the SCSS variable name that gets constructed as each level of the token tree is traversed
+ * @returns {string[]} an array of scss variable design tokens
+ */
+const parseTokens = (tokensNode, constructedTokenName = null) => {
+  const scssVariables = []
+  let tokenName = '';
 
-  if (!tokens) {
+  if (!tokensNode) {
     return;
   }
 
-  if (typeof tokens === 'object') {
-    const nodes = Object.keys(tokens)
-    nodes.forEach(n => {
-      
-      name = label 
-      ? `${label}-${n}` 
-      : n;
+  if (typeof tokensNode === 'object') {
+    const nodes = Object.keys(tokensNode)
+    nodes.forEach(node => {
+      tokenName = constructedTokenName 
+      ? `${constructedTokenName}-${node}` 
+      : node;
 
-      if (typeof tokens[n] === 'string' || typeof tokens[n] === "number") {
-        arr.push(`$${name}: ${tokens[n]};`);
+      if (typeof tokensNode[node] === 'string' || typeof tokensNode[node] === "number") {
+        scssVariables.push(`$${tokenName}: ${tokensNode[node]};`);
       }
 
-      if (typeof tokens[n] === 'object') {
-        arr.push(parseTokens(tokens[n], name))
+      if (typeof tokensNode[node] === 'object') {
+        scssVariables.push(parseTokens(tokensNode[node], tokenName))
       }
     });
   }
 
-  return arr.flat();
+  return scssVariables.flat();
 }
 
 const writeToFile = (scssVariables) => {
@@ -39,14 +44,13 @@ const buildTokens = () => {
   const content = fs.readFileSync('./src/tokens.json')
   const tokens = JSON.parse(content);
   const scssVariables = parseTokens(tokens);
-  writeToFile(scssVariables);
+
+  return scssVariables;
 }
-
-
 
 const generateScssFile = () => {
-  buildTokens();
+  const scssVariablesString = buildTokens();
+  writeToFile(scssVariablesString);
 }
-
 
 generateScssFile();
